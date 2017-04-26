e37ba89ac72c1291f8176eb289fa9e93fd6feb88
Bruno <brunoteixeiralc@gmail.com>
2017-04-25 22:27:23 -0300

    commit 25/04

--- Momentos/FIRImage.swift
+++ Momentos/FIRImage.swift
@@ -25,7 +25,58 @@ extension FIRImage{
     
     func saveProfileImage(userUID:String,completion:@escaping (Error?) -> Void){
         
+        let resized = image.resize()
+        let imageData =  UIImageJPEGRepresentation(resized, 0.9)
+        
+        ref = StorageReference.profileImages.ref().child(userUID)
+        downloadLink = ref.description
+        
+        ref.put(imageData!, metadata: nil) { (metadData, error) in
+           completion(error)
+        }
     }
+    
+    func save(uid:String, completion:@escaping (Error?) -> Void){
+        
+        let resized = image.resize()
+        let imageData =  UIImageJPEGRepresentation(resized, 0.9)
+        
+        ref = StorageReference.images.ref().child(uid)
+        downloadLink = ref.description
+        
+        ref.put(imageData!, metadata: nil) { (metadData, error) in
+            completion(error)
+        }
+
+    }
+}
+
+extension FIRImage{
+    
+    class func downloadProfileImage(uid:String,completion:@escaping (UIImage?,Error?) -> Void){
+        
+        StorageReference.profileImages.ref().child(uid).data(withMaxSize: 1*1024*1024) {(imageData, error) in
+            if error == nil && imageData != nil{
+                let image = UIImage(data: imageData!)
+                completion(image,error)
+            }else{
+                completion(nil,error)
+            }
+        }
+   }
+    
+    class func downloadImage(uid:String,completion:@escaping (UIImage?,Error?) -> Void){
+        
+        StorageReference.images.ref().child(uid).data(withMaxSize: 1*1024*1024) {(imageData, error) in
+            if error == nil && imageData != nil{
+                let image = UIImage(data: imageData!)
+                completion(image,error)
+            }else{
+                completion(nil,error)
+            }
+        }
+    }
+    
 }
 
 private extension UIImage{
--- Momentos/User.swift
+++ Momentos/User.swift
@@ -16,25 +16,53 @@ class User {
     var fullName:String
     var bio:String
     var website:String
-    var profile:UIImage?
+    var profileImage:UIImage?
     
     var follows:[User]
     var followedBy: [User]
     
-    init(uid:String,userName:String,fullName:String,bio:String,website:String,profile:UIImage,follows:[User],followedBy:[User]) {
+    init(uid:String,userName:String,fullName:String,bio:String,website:String,profileImage:UIImage,follows:[User],followedBy:[User]) {
         
         self.uid = uid
         self.userName = userName
         self.fullName = fullName
         self.bio = bio
         self.website = website
-        self.profile = profile
+        self.profileImage = profileImage
         self.follows = follows
         self.followedBy = followedBy
     
     }
     
-    func save(){
+    init(dictionary:[String:Any]) {
+        
+        uid = dictionary["uid"] as! String
+        userName = dictionary["username"] as! String
+        fullName = dictionary["fullname"] as! String
+        bio = dictionary["bio"] as! String
+        website = dictionary["website"] as! String
+        
+        self.follows = []
+        if let followDict = dictionary["follows"] as? [String:Any]{
+            for(_,userDict) in followDict{
+                if let userDict = userDict as? [String:Any]{
+                    self.follows.append(User(dictionary:userDict))
+                }
+            }
+        }
+        
+        self.followedBy = []
+        if let followedByDict = dictionary["followedBy"] as? [String:Any]{
+            for(_,userDict) in followedByDict{
+                if let userDict = userDict as? [String:Any]{
+                    self.followedBy.append(User(dictionary:userDict))
+                }
+            }
+        }
+
+    }
+
+    func save(completion:@escaping (Error?) -> Void){
         
         let ref = DatabaseReference.user(uid:uid).ref()
         ref.setValue(toDictionary())
@@ -45,6 +73,15 @@ class User {
         
         for user in followedBy{
             ref.child("followedBy/\(user.uid)").setValue(user.toDictionary())
+        }
+        
+        if let profileImage = self.profileImage{
+            
+            let firImage = FIRImage(image: profileImage)
+            firImage.saveProfileImage(userUID: self.uid, completion: { (error) in
+                completion(error)
+            })
+            
         }
         
     }
